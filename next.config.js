e127266540a3e7ee4e7714f87b5605d98a72c4cd
mdrafi10 <mdrafi101010101010@gmail.com>
2022-03-03 04:36:44 +0600

    Deploy discord

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "encrypted-tbn0.gstatic.com",
+      "cdn.sanity.io",
+      "www.apkmirror.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
